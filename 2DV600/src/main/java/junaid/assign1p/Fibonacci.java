15f31f043c491ba2cfc283ac9ed47c480dc14411
junaidraza45 <junaidraza453@gmail.com>
2018-09-23 16:32:59 +0500

    Added few changes to fabnoci sequence

--- 2DV600/src/main/java/junaid/assign1p/Fibonacci.java
+++ 2DV600/src/main/java/junaid/assign1p/Fibonacci.java
@@ -7,16 +7,24 @@ public class Fibonacci {
         int[] feb = new int[Fibcnt];
         feb[0] = 0;
         feb[1] = 1;
-        for (int i = 2; i < Fibcnt; i++) {
+        for (int i = 2; i < Fibcnt ; i++) {
             feb[i] = feb[i - 1] + feb[i - 2];
+            if(feb[i] < 1000 ) {
+                System.out.print(feb[i] + " ");
+            }else{
+                break;
+            }
         }
+
+
+
 
         int sum_odd = 0;
 
         for (int i = 0; i < feb.length; i++) {
-            if (feb[i] % 2 == 1) {
+            if (feb[i] % 2 == 1 && feb[i] < 1000) {
                 sum_odd += feb[i];
-                System.out.print(feb[i] + " ");
+                //System.out.print(feb[i] + " ");
             }
         }
         System.out.println();
